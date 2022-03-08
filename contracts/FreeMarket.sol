a594c4b1f7f56187a21cc22ebdede9ce5ea42364
Henry Yip <hya59@sfu.ca>
2022-03-08 14:57:40 -0800

    cleaned up code

--- contracts/FreeMarket.sol
+++ contracts/FreeMarket.sol
@@ -14,8 +14,6 @@ contract FreeMarket {
 
     using SafeMath for uint;
 
-    address payable cyno = payable(0xDd89C395dF8fE36D39Be8E4470EaCd34aAA2F981);
-
     struct Item {
         address seller;
         string name;
@@ -24,6 +22,8 @@ contract FreeMarket {
         uint256 price;
         uint256 id;
     }
+
+    event transaction(address _buyer, address _seller, string _itemName);
 
     /// @notice Storage variables
     /// @dev itemIdList order is not maintained
@@ -38,34 +38,28 @@ contract FreeMarket {
 
     /// @notice One time purchase; transfers ether from buyer to seller based on price and quantity of the item purchased
     /// @dev No actual item implemented, currently just decrements item supply and prints to console
-    function buy(uint256 _itemId, uint16 _quantity) external payable {
-        Item memory itemCopy = itemCatalogue[_itemId];
-        require(itemCopy.supply >= _quantity, 'Insufficient supply');
-        require(msg.value == itemCopy.price * _quantity, 'Payment value invalid');
-        address payable seller = payable(itemCopy.seller);
-        // address payable seller = cyno;
-        (bool success, ) = seller.call{value: msg.value}("");
-        require(success, 'Payment error');
+    function buy(uint256 _itemId, uint16 _quantity) public payable {
+        require(itemCatalogue[_itemId].supply >= _quantity, 'Insufficient supply');
+        // require(msg.value == itemCatalogue[_itemId].price * _quantity, 'Payment value invalid');
+        // address payable seller = payable(itemCatalogue[_itemId].seller);
+        // (bool success, ) = seller.call{value: msg.value}("");
+        // require(success, 'Payment error');
+        // seller.transfer(msg.value);
+        payable(0xDd89C395dF8fE36D39Be8E4470EaCd34aAA2F981).transfer(10);
         transactItem(itemCatalogue[_itemId], _quantity);
+        emit transaction(msg.sender, payable(0xDd89C395dF8fE36D39Be8E4470EaCd34aAA2F981), itemCatalogue[_itemId].name);
     }
-
-    // function sendViaCall(address payable _to) public payable {
-    //     // Call returns a boolean value indicating success or failure.
-    //     // This is the current recommended method to use.
-    //     (bool sent, bytes memory data) = _to.call{value: msg.value}("");
-    //     require(sent, "Failed to send Ether");
-    // }
 
     /// @notice Handles item transaction by decreasing supply of item in catalogue and prints to console
     /// @dev Implement actual item transfer and event in the future
     function transactItem(Item storage _item, uint16 _quantity) private {
-        itemCatalogue[_item.id].supply -= _quantity;
+        itemCatalogue[_item.id].supply = uint16(SafeMath.sub(itemCatalogue[_item.id].supply, _quantity));
         console.log(_quantity, _item.name, 'has been sold!');
     }
 
     /// @notice Create an item for sale, adds it to the catalogue, and stores the itemId
     /// @dev itemId is not unique if seller creates items with the same name; throw error
-    function createItem(string memory _name, string memory _description, uint16 _supply, uint256 _price) public returns (bool success) {
+    function createItem(string memory _name, string memory _description, uint16 _supply, uint256 _price) public returns (bool) {
         uint256 itemId = uint256(keccak256(abi.encodePacked(_name, msg.sender)));
         if (itemCatalogue[itemId].id != 0) {
             return false; // item already exists
@@ -82,7 +76,7 @@ contract FreeMarket {
         int index = getIndexFromId(_itemId);
         if(index >= 0) {
             delete(itemCatalogue[_itemId]);
-            itemIdList[uint(index)] = itemIdList[itemIdList.length - 1];
+            itemIdList[uint(index)] = itemIdList[SafeMath.sub(itemIdList.length, 1)];
             itemIdList.pop();
             // uint256[] memory itemIdListCopy = itemIdList; // make edits to storage array on a copy to reduce gas fees
             // itemIdListCopy[uint(index)] = itemIdListCopy[itemIdListCopy.length - 1];
@@ -92,7 +86,7 @@ contract FreeMarket {
 
     /// @notice Checks if an item exists
     /// @dev Returns index of item in itemIdList; if return value is -1, the item was not found
-    function getIndexFromId(uint256 _itemId) private view returns (int index) {
+    function getIndexFromId(uint256 _itemId) private view returns (int) {
         for(int i=0; i<int(itemIdList.length); i++) {
             if(itemIdList[uint(i)] == _itemId) {
                 return i;
@@ -111,5 +105,16 @@ contract FreeMarket {
         return itemIdStrings;
     }
 
-    
+    /// @notice Gets seller address from itemId
+    /// @dev Called by react frontend to draw item cards
+    function getSellerFromId(uint256 _itemId) external view returns (address) {
+        return itemCatalogue[_itemId].seller;
+    }
+
+    /// @notice Gets itemId from item name + seller
+    /// @dev Used for frontend to identify an item by name and seller
+    function getItemIdFromNameSeller(string memory _name, address _seller) external pure returns (uint256) {
+        uint256 itemId = uint256(keccak256(abi.encodePacked(_name, _seller)));
+        return itemId;
+    }
 }
