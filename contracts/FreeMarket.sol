511655e7c6d2d0e182a4903631af8b04a3f24ff4
Henry Yip <hya59@sfu.ca>
2022-03-08 19:42:54 -0800

    Updated to v1, simplified contract architecture

--- contracts/FreeMarket.sol
+++ contracts/FreeMarket.sol
@@ -1,120 +1,154 @@
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2; // for passing structs into function parameters
+pragma experimental ABIEncoderV2; // For passing structs into function parameters
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
 
-/// @title Digital shop
+/// @title FreeMarket
 /// @author Henry Yip 
-/// @notice Contract should allow users to purchase an item with ether and post products for sale 
+/// @notice Contract should allow users to transact Ether and list merchandise for sale 
 contract FreeMarket {
 
     using SafeMath for uint;
+    using Counters for Counters.Counter;
 
-    struct Item {
+    Counters.Counter private merchandiseIds; // Simple unique merchandise identifier
+    Counters.Counter private merchandiseExpired; // Tracks number of sold out or removed merchandise for array size declarations
+
+    /// @notice FreeMarket contract owner
+    address payable owner;
+
+    /// @notice Merchandise catalogue 
+    /// @dev Maps merchandiseId -> Merchandise
+    mapping(uint256 => Merchandise) private idToMerchandise;
+
+    /// @notice Merchandise item & metadata to store on blockchain
+    /// @dev Can scale to other types of merchandise i.e. NFTs
+    struct Merchandise {
         address seller;
-        string name;
-        string description;
         uint16 supply;
         uint256 price;
-        uint256 id;
+        uint256 merchandiseId;
     }
 
-    event transaction(address _buyer, address _seller, string _itemName);
+    event MerchandiseAdded (
+        address _seller,
+        uint16 _supply,
+        uint256 _price,
+        uint256 _merchandiseId
+    );
 
-    /// @notice Storage variables
-    /// @dev itemIdList order is not maintained
-    uint256[] public itemIdList; // array of IDs per item for sale
-    mapping (uint256 => Item) public itemCatalogue;  // mapping of item IDs to item
+    event Transaction(
+        address _seller, 
+        address _buyer, 
+        uint16 _quantity,
+        uint256 _merchandiseId
+    );
 
-    /// @notice Modifier for sellers to manage their own items
-    modifier onlySeller(Item memory _item) {
-        require(msg.sender == _item.seller);
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /// @notice For sellers to manage their own items
+    modifier onlySeller(Merchandise memory _merchandise) {
+        require(msg.sender == _merchandise.seller);
         _;
     }
 
-    /// @notice One time purchase; transfers ether from buyer to seller based on price and quantity of the item purchased
-    /// @dev No actual item implemented, currently just decrements item supply and prints to console
-    function buy(uint256 _itemId, uint16 _quantity) public payable {
-        require(itemCatalogue[_itemId].supply >= _quantity, 'Insufficient supply');
-        // require(msg.value == itemCatalogue[_itemId].price * _quantity, 'Payment value invalid');
-        // address payable seller = payable(itemCatalogue[_itemId].seller);
-        // (bool success, ) = seller.call{value: msg.value}("");
-        // require(success, 'Payment error');
-        // seller.transfer(msg.value);
-        payable(0xDd89C395dF8fE36D39Be8E4470EaCd34aAA2F981).transfer(10);
-        transactItem(itemCatalogue[_itemId], _quantity);
-        emit transaction(msg.sender, payable(0xDd89C395dF8fE36D39Be8E4470EaCd34aAA2F981), itemCatalogue[_itemId].name);
+    /// @notice Transfers Ether from buyer to seller based on price and quantity of the merchandise purchased
+    /// @dev TODO Issue receipts for both buyer and seller 
+    function transactMerchandise(uint256 _merchandiseId, uint16 _quantity) external payable {
+        require(idToMerchandise[_merchandiseId].supply >= _quantity, 'Insufficient supply');
+        require(msg.value == idToMerchandise[_merchandiseId].price * _quantity, 'Invalid payment value');
+        
+        // Transfer Ether
+        (bool success, ) = payable(idToMerchandise[_merchandiseId].seller).call{value: msg.value}("");
+        require(success, 'Payment error');
+
+        // Reflect transaction in FreeMarket
+        idToMerchandise[_merchandiseId].supply = uint16(SafeMath.sub(idToMerchandise[_merchandiseId].supply, _quantity));
+        merchandiseExpired.increment();
+        emit Transaction(idToMerchandise[_merchandiseId].seller, msg.sender, _quantity, _merchandiseId);
     }
 
-    /// @notice Handles item transaction by decreasing supply of item in catalogue and prints to console
-    /// @dev Implement actual item transfer and event in the future
-    function transactItem(Item storage _item, uint16 _quantity) private {
-        itemCatalogue[_item.id].supply = uint16(SafeMath.sub(itemCatalogue[_item.id].supply, _quantity));
-        console.log(_quantity, _item.name, 'has been sold!');
+    /// @notice List merchandise for sale
+    /// @dev Additional merchandise metadata (description, images, etc.) are not stored on chain
+    function addMerchandise(uint16 _supply, uint256 _price) external {
+        
+        // Assign unique ID to merchandise
+        merchandiseIds.increment();
+        uint256 merchandiseId = merchandiseIds.current();
+
+        // Add merchandise to FreeMarket
+        idToMerchandise[merchandiseId] = Merchandise(
+            msg.sender,
+            _supply,
+            _price,
+            merchandiseId
+        );
+        emit MerchandiseAdded(msg.sender, _supply, _price, merchandiseId);
     }
 
-    /// @notice Create an item for sale, adds it to the catalogue, and stores the itemId
-    /// @dev itemId is not unique if seller creates items with the same name; throw error
-    function createItem(string memory _name, string memory _description, uint16 _supply, uint256 _price) public returns (bool) {
-        uint256 itemId = uint256(keccak256(abi.encodePacked(_name, msg.sender)));
-        if (itemCatalogue[itemId].id != 0) {
-            return false; // item already exists
-        } else {
-            itemCatalogue[itemId] = Item(msg.sender, _name, _description, _supply, _price, itemId);
-            itemIdList.push(itemId);
-            return true;
-        }
+    /// @notice For sellers to remove item from FreeMarket
+    function removeMerchandise(uint256 _merchandiseId) external onlySeller(idToMerchandise[_merchandiseId]) {
+        delete(idToMerchandise[_merchandiseId]);
+        merchandiseExpired.increment();
     }
 
-    /// @notice Remove item from catalogue
-    /// @dev Only the seller can remove their item from the catalogue
-    function removeItem(uint256 _itemId) public onlySeller(itemCatalogue[_itemId]) {
-        int index = getIndexFromId(_itemId);
-        if(index >= 0) {
-            delete(itemCatalogue[_itemId]);
-            itemIdList[uint(index)] = itemIdList[SafeMath.sub(itemIdList.length, 1)];
-            itemIdList.pop();
-            // uint256[] memory itemIdListCopy = itemIdList; // make edits to storage array on a copy to reduce gas fees
-            // itemIdListCopy[uint(index)] = itemIdListCopy[itemIdListCopy.length - 1];
-            // itemIdList = itemIdListCopy.pop(); // cannot pop, splice from memory array :(
-        }
-    }
+    /// @notice Gets all available merchandise and returns it in an array
+    /// @dev Will have to scale to allow for filtered searches or find another solution when too many merchandise
+    function fetchMerchandiseAll() public view returns (Merchandise[] memory) {
+        
+        // For array & loop tracking
+        uint merchandiseIdCount = merchandiseIds.current();
+        uint merchandiseCount = SafeMath.sub(merchandiseIds.current(), merchandiseExpired.current());
+        uint currentIndex = 0;
 
-    /// @notice Checks if an item exists
-    /// @dev Returns index of item in itemIdList; if return value is -1, the item was not found
-    function getIndexFromId(uint256 _itemId) private view returns (int) {
-        for(int i=0; i<int(itemIdList.length); i++) {
-            if(itemIdList[uint(i)] == _itemId) {
-                return i;
+        // Loop through merchandise in FreeMarket to save in-stock merchandise into an array
+        Merchandise[] memory merchandise = new Merchandise[](merchandiseCount);
+        for(uint i=0; i<merchandiseIdCount; i++) {
+            if(idToMerchandise[i+1].supply > 0) {
+                uint currentId = idToMerchandise[i+1].merchandiseId;
+                Merchandise storage currentMerchandise = idToMerchandise[currentId];
+                merchandise[currentIndex] = currentMerchandise;
+                currentIndex = SafeMath.add(currentIndex, 1);
             }
         }
-        return -1;
+
+        return merchandise;
     }
 
-    /// @notice Custom getters function for itemIdList 
-    /// @dev Returns list of strings instead for JS readability
-    function getItemIdList() public view returns (string[] memory) {
-        string[] memory itemIdStrings = new string[](itemIdList.length);
-        for(uint i=0; i<itemIdList.length; i++) {
-            itemIdStrings[i] = (Strings.toString(itemIdList[i]));
+    /// @notice Gets merchandise from a specified seller
+    function fetchMerchandiseFrom(address _seller) public view returns (Merchandise[] memory) {
+        
+        // For array & loop tracking
+        uint merchandiseIdCount = merchandiseIds.current();
+        uint merchandiseCount = 0;
+        uint currentIndex = 0;
+
+        // Determine return array size
+        for(uint i=0; i<merchandiseIdCount; i++) {
+            if(idToMerchandise[i+1].seller == _seller) {
+                merchandiseCount = SafeMath.add(merchandiseCount, 1);
+            }
         }
-        return itemIdStrings;
+
+        // Loop through FreeMarket to save merchandise from specified seller into array
+        Merchandise[] memory merchandise = new Merchandise[](merchandiseCount);
+        for(uint i=0; i<merchandiseIdCount; i++) {
+            if(idToMerchandise[i+1].seller == _seller) {
+                uint currentId = idToMerchandise[i+1].merchandiseId;
+                Merchandise storage currentMerchandise = idToMerchandise[currentId];
+                merchandise[currentIndex] = currentMerchandise;
+                currentIndex = SafeMath.add(currentIndex, 1);
+            }
+        }
+
+        return merchandise;
     }
 
-    /// @notice Gets seller address from itemId
-    /// @dev Called by react frontend to draw item cards
-    function getSellerFromId(uint256 _itemId) external view returns (address) {
-        return itemCatalogue[_itemId].seller;
-    }
-
-    /// @notice Gets itemId from item name + seller
-    /// @dev Used for frontend to identify an item by name and seller
-    function getItemIdFromNameSeller(string memory _name, address _seller) external pure returns (uint256) {
-        uint256 itemId = uint256(keccak256(abi.encodePacked(_name, _seller)));
-        return itemId;
-    }
 }
